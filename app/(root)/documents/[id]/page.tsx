e1f9236014eb57c0079a7f9b06cc5a8405e92038
Hardik <hardiknahta111@gmail.com>
2025-03-12 09:55:02 +0530

    Integrated Liveblocks authentication and added room access functionality

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { getDocument } from "@/lib/actions/room.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Document = () => {
-  return <div></div>;
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  return (
+    <main className="flex w-full flex-col">
+      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+    </main>
+  );
 };
 
 export default Document;
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,33 +1,37 @@
 "use client";
 
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
-import React from "react";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ActiveCollaborators from "./ActiveCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
   return (
-    <div>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-          <div className="collaborative-room">
-            <Header>
-              <div className="flex w-fit items-center justify-center gap-2">
-                <p className="document-title"> Share </p>
-              </div>
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title"> Share </p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
               <SignedOut>
                 <SignInButton />
               </SignedOut>
               <SignedIn>
                 <UserButton />
               </SignedIn>
-            </Header>
-            <Editor />
-          </div>
-        </ClientSideSuspense>
-      </RoomProvider>
-    </div>
+            </div>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
   );
 };
 
